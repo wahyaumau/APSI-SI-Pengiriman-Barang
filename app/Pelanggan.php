893bc42990062451bd104cac054a834d1a0a0662
wahyaumau <wahyaumau@gmail.com>
2019-06-23 20:03:26 +0700

    preparing model and migration for multi auth

---
+++ app/Pelanggan.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Pelanggan extends Authenticatable
+{
+    use Notifiable;
+
+    protected $table = "pelanggan";
+    public $timestamps = false;
+    protected $primaryKey = 'kode_pelanggan';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nama', 'email', 'password', 'alamat', 'telepon',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
---
+++ database/migrations/2019_06_23_125639_create_owner_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOwnerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('owner', function (Blueprint $table) {
+            $table->string('kode_owner', 10)->primary();
+            $table->string('nama');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->text('alamat');
+            $table->string('telepon', 12);
+            $table->rememberToken();            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('owner');
+    }
+}
